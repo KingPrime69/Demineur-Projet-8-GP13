3117833b7461f1d874f6a208f2ee1b7a4ac36807
KingPrime69 <quentin.ripot@free.fr>
2023-02-22 13:56:55 +0100

    Spawn Grid

--- Assets/SpawnerTile.cs
+++ Assets/SpawnerTile.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class SpawnerTile : MonoBehaviour
 {
+    [SerializeField] GameObject original;
+    [SerializeField] Vector2 Size;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < Size.x; i++)
+        {
+            for(int y = 0; y < Size.y; y++)
+            {
+                Instantiate(original, new Vector3(i, y), Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
