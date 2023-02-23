48bd8cbf2206b96662ab1b90ab08ed2ca0aa7195
Raphaël Mateescu <73947667+SmartNewt@users.noreply.github.com>
2023-02-23 11:37:07 +0100

    fixed bomb superposition

--- Assets/SpawnerTile.cs
+++ Assets/SpawnerTile.cs
@@ -10,31 +10,40 @@ public class SpawnerTile : MonoBehaviour
 
     [SerializeField] int _nbBombs;
     // Start is called before the first frame update
-    
+    public bool[,] array;
+
     void Start()
     {
-        bool[,] array = new bool[(int)_Size.x, (int)_Size.y];
+         array = new bool[(int)_Size.x, (int)_Size.y];
 
         InitArray(array);
 
         for (int i = 0; i < _Size.x; i++)
         {
-            for(int y = 0; y < _Size.y; y++)
+            for (int y = 0; y < _Size.y; y++)
             {
-                
+
                 GameObject go = Instantiate(_original, new Vector3(i, y), Quaternion.identity);
-                if (array[i,y]) go.GetComponent<tileBehaviour>().SetBomb();
+                if (array[i, y]) go.GetComponent<tileBehaviour>().SetBomb();
             }
         }
     }
 
     void InitArray(bool[,] array)
     {
-        for (int i = 0; i < _nbBombs; i++)
+        if (_nbBombs < _Size.x * _Size.y)
         {
-            int x = Random.Range(0, (int)_Size.x);
-            int y = Random.Range(0, (int)_Size.y);
-            array[x, y] = true;
+            for (int i = 0; i < _nbBombs; i++)
+            {
+                int x, y;
+                do
+                {
+                    x = Random.Range(0, (int)_Size.x);
+                    y = Random.Range(0, (int)_Size.y);
+                }
+                while (!array[x, y]);
+                array[x, y] = true;
+            }
         }
     }
 }
